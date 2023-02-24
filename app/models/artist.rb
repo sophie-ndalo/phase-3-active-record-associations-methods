384b218e34d4b7e63bd6a6593bd868a1be0cc643
sophia <sophiendalo@gmail.com>
2023-02-24 17:12:34 +0300

    completed assignment

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,18 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    first_song = Song.order(:created_at).first
+    first_song.genre if first_song
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.all.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.genres.all.count
   end
+
 end
